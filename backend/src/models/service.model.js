52836bd45313dc83177fd7707034321b85b230e0
Varga Jenő <jeno_varga@yahoo.com>
2021-08-09 14:13:35 +0200

    feladat-1

--- backend/src/models/service.model.js
+++ backend/src/models/service.model.js
@@ -1,1 +1,45 @@
 // 1. Feladat: pótold a service modellt.
+const mongoose = require('mongoose');
+
+const ServiceSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    since: {
+        type: Number,
+        required: true
+    },
+    street: {
+        type: String,
+        default: 'not specified',
+    },
+    city: {
+        type: String,
+        default: 'not specified',
+    },
+    country: {
+        type: String,
+        default: 'not specified',
+    },
+    active: {
+        type: Boolean,
+        default: true
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('customer', ServiceSchema);
+
+/*
+export class Service {
+  _id: string = '';
+  name: string = '';
+  since: number = 0;
+  street?: string = '';
+  city?: string = '';
+  country?: string = '';
+  active?: boolean = true;
+}
+*/
