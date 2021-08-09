001a70c641a2f523e72b2b12bee694b641fc5092
Varga Jenő <jeno_varga@yahoo.com>
2021-08-09 15:13:58 +0200

    feladat-4

--- frontend/src/app/page/customer/customer.component.ts
+++ frontend/src/app/page/customer/customer.component.ts
@@ -10,7 +10,11 @@ import { CustomerService } from 'src/app/service/customer.service';
 })
 export class CustomerComponent implements OnInit {
 
-  constructor() { }
+  list$: Observable<Customer[]> = this.carService.getAll();
+
+  constructor(
+    private carService: CustomerService,
+  ) { }
 
   ngOnInit(): void {
   }
